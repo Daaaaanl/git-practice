33a66a80fc9c41ff712755c1c49d94a298f422ce
Dan Li <danli@hawaii.edu>
2016-10-15 02:02:26 +0000

    Add Student.java to project.

---
+++ Student.java
@@ -0,0 +1,17 @@
+public class Student {
+  int age;
+  gfloat gpa;
+  String firstName;
+  String lastName;
+  String major;
+
+  public Student() {
+  }
+
+
+  public Student(String firstName, String lastName, int age) {
+    this.firstName = firstName;
+    this.lastName  = lastName;
+    this.age       = age;
+  }
+}
